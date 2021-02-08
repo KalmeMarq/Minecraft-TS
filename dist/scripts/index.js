19025d458752742a81278c2481d29e0d180dc28d
Kalme <55203647+KalmeMarq@users.noreply.github.com>
2021-02-08 12:41:40 +0000

    fixed req 4

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -208,9 +208,9 @@ define("utils/Resources", ["require", "exports", "utils/JSONUtils"], function (r
                 });
             }
         });
-        await JSONUtils_js_2.default.getJSONFile('ui/_ui_defs.json', ((data) => {
+        await JSONUtils_js_2.default.getJSONFile('src/ui/_ui_defs.json', ((data) => {
             data.ui_defs.forEach(async (file) => {
-                await JSONUtils_js_2.default.getJSONFile('' + file, ((dataa) => {
+                await JSONUtils_js_2.default.getJSONFile('src/' + file, ((dataa) => {
                     if (!exports.MCUI[dataa.namespace]) {
                         exports.MCUI[dataa.namespace] = dataa;
                     }
--- src/utils/Resources.ts
+++ src/utils/Resources.ts
@@ -78,9 +78,9 @@ export async function getAllResources() {
     }
   });
 
-  await JSONUtils.getJSONFile('ui/_ui_defs.json', ((data: any) => {
+  await JSONUtils.getJSONFile('src/ui/_ui_defs.json', ((data: any) => {
     data.ui_defs.forEach(async (file: any) => {
-      await JSONUtils.getJSONFile('' + file, ((dataa: any) => {
+      await JSONUtils.getJSONFile('src/' + file, ((dataa: any) => {
         if(!MCUI[dataa.namespace]) {
           MCUI[dataa.namespace] = dataa;
         }
